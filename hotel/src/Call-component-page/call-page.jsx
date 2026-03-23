6cf11e1753429af902500833ff17c0f5e8deb103
abdullahxr65-ops <abdullahxr195@gmail.com>
2026-03-24 02:22:19 +0300

    hu

---
+++ hotel/src/Call-component-page/call-page.jsx
@@ -0,0 +1,15 @@
+import MenuCardEvent from './component/Event page/Menu-Card-Event'
+import ListCardEvent from './component/Event page/List-Card-Event'
+
+export default function CallPage( ) {
+
+return(
+<>
+
+<ListCardEvent/>
+<MenuCardEvent/>
+
+</>
+
+)
+}
