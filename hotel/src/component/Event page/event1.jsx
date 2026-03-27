76feb5238433e84efba98be4e5048f64b5d188d0
abdullahxr65-ops <abdullahxr195@gmail.com>
2026-03-27 05:32:32 +0300

    ...

--- hotel/src/component/Event page/event1.jsx
+++ hotel/src/component/Event page/event1.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
+
+import {Card , CardActionArea, CardMedia, CardContent , Typography ,Button} from '@mui/material';
 export default function Event1() {
     return (
         <>
--- hotel/src/component/Event page/event2.jsx
+++ hotel/src/component/Event page/event2.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 
 export default function Event2() {
     return (
